09d039ef1da57ac532b03cdf56bcce19032c2519
Dmitry Malofeev <dima.malofeev@lepshey.ru>
2016-07-12 17:03:22 +0300

    array method filter implemented

--- homework#1/index.js
+++ homework#1/index.js
@@ -13,6 +13,25 @@ let getArrayMethods = function() {
                 }
 
             }
+        },
+        filter: function(source, callback) {
+            var sourceLength = source.length,
+                i = 0,
+                newArray = [];
+
+            for (i; i < sourceLength; i++) {
+
+                if (source[i] !== undefined) {
+
+                    if (callback(source[i], i, source)) {
+                        newArray.push(source[i]);
+                    }
+
+                }
+
+            }
+
+            return newArray;
         }
     };
 };
--- homework#1/test.js
+++ homework#1/test.js
@@ -4,6 +4,11 @@ var log = function(element, index, array) {
     console.log(element);
 };
 
+var biggerThan = function(value) {
+    return value > 4;
+};
+
 var array = [1, 2, 3, 4, 5, 6];
 
 console.log(arrayMethods.forEach(array, log));
+console.log(arrayMethods.filter(array, biggerThan));
