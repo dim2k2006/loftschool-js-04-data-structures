0e73c87b2918018e799299096ac781daa179c1f5
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-07-13 21:59:09 +0300

    array method map test ok

--- homework#1/test.js
+++ homework#1/test.js
@@ -21,6 +21,6 @@ var array = [1, 2, 3, 4, 5, 6];
 
 // console.log(arrayMethods.forEach(array, log));
 // console.log(arrayMethods.filter(array, biggerThan));
-console.log(arrayMethods.map(array, square));
-// console.log(arrayMethods.reduce(array, sum));
+// console.log(arrayMethods.map(array, square));
+console.log(arrayMethods.reduce(array, sum));
 // console.log(arrayMethods.slice(array, -2, 3));
